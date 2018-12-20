f61479a1856629889a18c4c9c34b319797dafae1
happygirlzt <happygirlzt@gmail.com>
2018-12-20 22:51:30 +0800

    day 34

--- Java/TopKFrequentElements.java
+++ Java/TopKFrequentElements.java
@@ -1,4 +1,3 @@
-
 /*
  * LeetCode # 347. Top K Frequent Elements
  * happygirlzt
@@ -15,56 +14,102 @@ import java.util.HashSet;
 import java.util.Collection;
 
 public class TopKFrequentElements {
-	public static List<Integer> topKFrequent(int[] nums, int k) {
-		List<Integer> res = new ArrayList<>();
+  public static List<Integer> topKFrequent(int[] nums, int k) {
+    List<Integer> res = new ArrayList<>();
 
-		Arrays.sort(nums);
+    Arrays.sort(nums);
 
-		Set<Integer> set = new HashSet<>();
+    Set<Integer> set = new HashSet<>();
 
-		HashMap<Integer, Integer> map = new HashMap<>();
+    HashMap<Integer, Integer> map = new HashMap<>();
 
-		for (int i = 0; i < nums.length; i++) {
-			if (set.add(nums[i])) {
-				map.put(nums[i], 1);
-			} else {
-				int tmp = map.get(nums[i]);
-				map.remove(nums[i]);
-				map.put(nums[i], tmp + 1);
-			}
-		}
+    for (int i = 0; i < nums.length; i++) {
+      if (set.add(nums[i])) {
+        map.put(nums[i], 1);
+      } else {
+        int tmp = map.get(nums[i]);
+        map.remove(nums[i]);
+        map.put(nums[i], tmp + 1);
+      }
+    }
 
-		System.out.print(map);
-		Collection<Integer> val = map.values();
+    System.out.print(map);
+    Collection<Integer> val = map.values();
 
-		List<Integer> a = new ArrayList<>();
+    List<Integer> a = new ArrayList<>();
 
-		for (Integer j : val) {
-			a.add(j);
-		}
-		Collections.sort(a);
-		Collections.reverse(a);
+    for (Integer j : val) {
+      a.add(j);
+    }
+    Collections.sort(a);
+    Collections.reverse(a);
 
-		System.out.print(a);
+    System.out.print(a);
 
-		Set<Integer> s = new HashSet<>();
+    Set<Integer> s = new HashSet<>();
 
-		for (int m = 0; m < k; m++) {
-			for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-				if (entry.getValue().equals(a.get(m))) {
-					if (s.add(entry.getKey())) {
-						res.add(entry.getKey());
-						System.out.println("Add a key" + entry.getKey());
-					}
-				}
-			}
-		}
+    for (int m = 0; m < k; m++) {
+      for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        if (entry.getValue().equals(a.get(m))) {
+          if (s.add(entry.getKey())) {
+            res.add(entry.getKey());
+            System.out.println("Add a key" + entry.getKey());
+          }
+        }
+      }
+    }
 
-		return res;
-	}
+    return res;
+  }
 
-	public static void main(String[] args) {
-		int[] a = { 1, 1, 2, 2, 2, 1 };
-		System.out.println(topKFrequent(a, 1));
-	}
+    // Updated on 20 Dec 2018, bucket sort
+    public List<Integer> topKFrequent1(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        List<Integer>[] bucket = new ArrayList[nums.length + 1];
+        for (int i : map.keySet()) {
+            if (bucket[map.get(i)] == null) {
+                bucket[map.get(i)] = new ArrayList<>();
+            }
+            bucket[map.get(i)].add(i);
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (int i = bucket.length - 1; i >= 1 && res.size() < k; i--) {
+            if (bucket[i] != null) {
+                int size = k;
+                for (int item: bucket[i]) {
+                    res.add(item);
+                    size--;
+                    if (size == 0) break;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    // Updated on 20 Dec 2018, heap
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>((i1, i2) -> map.get(i1) - map.get(i2));
+        for (int i : map.keySet()) {
+            heap.offer(i);
+            if (heap.size() > k) heap.poll();
+        }
+
+        LinkedList<Integer> res = new LinkedList<>();
+        while (heap.size() > 0) {
+            res.add(heap.poll());
+        }
+
+        return res;
+    }
 }
--- Java/TopKFrequentWords.java
+++ Java/TopKFrequentWords.java
@@ -19,21 +19,74 @@ public class TopKFrequentWords {
         PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
                                                                            (a,b) -> a.getValue()==b.getValue() ? a.getKey().compareTo(b.getKey()) : b.getValue()-a.getValue());
 
-        // PriorityQueue<Map.Entry<String, Integer>> minHeap = new PriorityQueue<Map.Entry<String, Integer>>(new Comparator<Map.Entry<String, Integer>>() {
-        //         @Override
-        //         public int compare(Map.Entry<String, Integer> entry1, Map.Entry<String, Integer> entry2) {
-        //             if(entry1.getValue() == entry2.getValue()) {
-        //                 return entry2.getKey().compareTo(entry1.getKey());
-        //             }
-        //             return entry1.getValue() - entry2.getValue();
-        //         }
-        //     });
+
         for (Map.Entry<String, Integer> entry: map.entrySet()) {
             pq.offer(entry);
         }
 
         while (res.size() < k) {
             res.add(pq.poll().getKey());
+        }
+
+        return res;
+    }
+
+    // Updated on 20 Dec 2018, Trie
+    class TrieNode {
+        TrieNode[] children;
+        boolean isWord;
+        String word;
+        public TrieNode() {
+            children = new TrieNode[26];
+        }
+    }
+
+    private void addWord(TrieNode root, String word) {
+        TrieNode cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (cur.children[index] == null) {
+                cur.children[index] = new TrieNode();
+            }
+            cur = cur.children[index];
+        }
+
+        cur.word = word;
+        cur.isWord = true;
+    }
+
+    private void getWords(TrieNode root, List<String> res, int k) {
+        if (root == null) return;
+        if (root.isWord) res.add(root.word);
+        if (res.size() == k) return;
+        for (int i = 0; i < 26; i++) {
+            if (root.children[i] != null) {
+                getWords(root.children[i], res, k);
+            }
+        }
+    }
+
+    public List<String> topKFrequent1(String[] words, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String word: words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        TrieNode[] count = new TrieNode[words.length + 1];
+        for (String word: map.keySet()) {
+            int freq = map.get(word);
+            if (count[freq] == null) {
+                count[freq] = new TrieNode();
+            }
+
+            addWord(count[freq], word);
+        }
+
+        // get k frequent words
+        LinkedList<String> res = new LinkedList<>();
+        for (int i = count.length - 1; i >= 1; i--) {
+            if (count[i] == null) continue;
+            getWords(count[i], res, k);
         }
 
         return res;
