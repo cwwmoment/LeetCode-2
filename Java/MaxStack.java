1c8786864b8508809a1e1bc2648b153859d05b63
happygirlzt <happygirlzt@gmail.com>
2019-04-04 23:45:05 +0800

    day 21

--- Java/MaxStack.java
+++ Java/MaxStack.java
@@ -59,3 +59,97 @@ class MaxStack {
  * int param_4 = obj.peekMax();
  * int param_5 = obj.popMax();
  */
+
+class MaxStack {
+    class Node {
+        int val;
+        Node next;
+        Node prev;
+        public Node(int v) {
+            val = v;
+            prev = null;
+            next = null;
+        }
+    }
+
+    Node head;
+    Node maxHead;
+    public MaxStack() {
+        head = null;
+        maxHead = null;
+    }
+
+    public void push(int x) {
+        Node n = new Node(x);
+        if (head == null) {
+            head = n;
+            maxHead = n;
+        } else {
+            n.next = head;
+            head.prev = n;
+            head = n;
+            if (n.val >= maxHead.val) {
+                maxHead = n;
+            }
+        }
+    }
+
+    public int pop() {
+        int val = head.val;
+        if (head == maxHead) {
+            Node cur = head.next;
+            maxHead = cur;
+            while (cur != null) {
+                if (cur.val > maxHead.val) {
+                    maxHead = cur;
+                }
+                cur = cur.next;
+            }
+        }
+
+        head = head.next;
+        if (head != null) {
+            head.prev = null;
+        }
+
+        return val;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int peekMax() {
+        return maxHead.val;
+    }
+
+    public int popMax() {
+        int val = maxHead.val;
+
+        if (head == maxHead) {
+            head = head.next;
+            if (head != null) {
+                head.prev = null;
+            }
+        } else {
+            if (maxHead.next != null) {
+                maxHead.next.prev = maxHead.prev;
+            }
+
+            if (maxHead.prev != null) {
+                maxHead.prev.next = maxHead.next;
+            }
+        }
+
+        Node cur = head;
+        maxHead = cur;
+        while (cur != null) {
+            if (cur.val > maxHead.val) {
+                maxHead = cur;
+            }
+            cur = cur.next;
+        }
+
+        return val;
+    }
+}
