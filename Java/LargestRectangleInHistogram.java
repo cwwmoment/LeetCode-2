abb197b2eb91f4f00045f77e79c409b2326d91ad
happygirlzt <happygirlzt@gmail.com>
2019-04-11 19:38:01 +0800

    day 28

--- Java/LargestRectangleInHistogram.java
+++ Java/LargestRectangleInHistogram.java
@@ -1,51 +1,29 @@
 /**
  * LeetCode #84. Largest Rectangle in Histogram
- * @happygirlzt Created on 19 Aug 2018
+ *
+ * Created by happygirlzt on 11 Apr 2019
  *
  */
-import java.util.Stack;
-
 public class LargestRectangleInHistogram {
-    public static int largestRectangleArea(int[] heights) {
-        if (heights == null || heights.length == 0)
-            return 0;
+    public int largestRectangleArea(int[] heights) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int max = 0;
+        int curIndex = 0;
+        int n = heights.length;
+        stack.push(-1);
 
-        int maxArea = 0;
-        Stack<Integer> stack = new Stack<>();
-
-        int tp = 0; // the peek of the stack
-
-        for (int i = 0; i <= heights.length; i++) {
-            int h = (i == heights.length ? 0 : heights[i]);
-            if (stack.isEmpty() || heights[stack.peek()] <= h) {
-                stack.push(i); // push the index
-            } else {
-                tp = stack.peek();
-                stack.pop();
-
-                maxArea = Math.max(maxArea, heights[tp] * (stack.isEmpty() ? i : i - stack.peek() - 1));
-                i--;
+        while (curIndex < n) {
+            while (stack.peek() != -1 && heights[stack.peek()] >= heights[curIndex]) {
+                max = Math.max(max, heights[stack.pop()] * (curIndex - stack.peek() - 1));
             }
+
+            stack.push(curIndex++);
         }
 
-        return maxArea;
-    }
-
-    public static int largestRectangleArea1(int[] height) {
-        int len = height.length;
-        Stack<Integer> s = new Stack<Integer>();
-        int maxArea = 0;
-        for(int i = 0; i <= len; i++){
-            int h = (i == len ? 0 : height[i]);
-            if(s.isEmpty() || h >= height[s.peek()]){
-                s.push(i);
-            }else{
-                int tp = s.pop();
-                maxArea = Math.max(maxArea, height[tp] * (s.isEmpty() ? i : i - 1 - s.peek()));
-                i--;
-            }
+        while (stack.peek() != -1) {
+            max = Math.max(max, heights[stack.pop()] * (n - stack.peek() - 1));
         }
-        return maxArea;
-    }
 
+        return max;
+    }
 }
--- Java/TrappingRainWater.java
+++ Java/TrappingRainWater.java
@@ -1,84 +1,98 @@
-import java.util.Stack;
-
 /**
  * LeetCode 42. Trapping Rain Water
- * 
+ *
  * @happygirlzt Created on 19 Aug 2018
  */
 public class TrappingRainWater {
 
-	// Solution 0: Naive
-	public static int trap(int[] height) {
-		int maxArea = 0;
-		if (height == null || height.length == 0)
-			return maxArea;
+    // Updated on 11 Apr 2019
+    // Monotonic stack
+    public int trap(int[] height) {
+        int n = height.length;
+        Deque<Integer> stack = new ArrayDeque<>();
+        int res = 0;
+        int curIndex = 0;
+        while (curIndex < n) {
+            while (!stack.isEmpty() && height[stack.peek()] < height[curIndex]) {
+                int top = stack.pop();
+                if (stack.isEmpty()) break;
+                int h = Math.min(height[stack.peek()], height[curIndex]) - height[top];
+                int dist = curIndex - stack.peek() - 1;
+                res += dist * h;
+            }
 
-		int[] leftHighest = new int[height.length + 1];
-		leftHighest[0] = 0;
+            stack.push(curIndex++);
+        }
 
-		for (int i = 0; i < height.length; i++) {
-			leftHighest[i + 1] = Math.max(leftHighest[i], height[i]);
-		}
+        return res;
+    }
 
-		int rightHighest = 0;
-		for (int j = height.length - 1; j > 0; j--) {
-			rightHighest = Math.max(rightHighest, height[j]);
-			int min = Math.min(leftHighest[j], rightHighest);
-			maxArea += (min > height[j]) ? min - height[j] : 0;
-		}
+    // Solution 0: Naive
+    public static int trap(int[] height) {
+        int maxArea = 0;
+        if (height == null || height.length == 0)
+            return maxArea;
 
-		return maxArea;
-	}
+        int[] leftHighest = new int[height.length + 1];
+        leftHighest[0] = 0;
 
-	// Solution 1: Two Pointers
-	public static int trap1(int[] height) {
-		if (height == null || height.length == 0)
-			return 0;
-		int i = 0, j = height.length - 1;
-		int leftMax = height[i], rightMax = height[j];
-		int maxArea = 0;
+        for (int i = 0; i < height.length; i++) {
+            leftHighest[i + 1] = Math.max(leftHighest[i], height[i]);
+        }
 
-		while (i < j) {
-			if (height[i] < height[j]) {
-				leftMax = Math.max(height[i], leftMax);
-				maxArea += leftMax - height[i];
-				i++;
-			} else {
-				rightMax = Math.max(height[j], rightMax);
-				maxArea += rightMax - height[j];
-				j--;
-			}
-		}
+        int rightHighest = 0;
+        for (int j = height.length - 1; j > 0; j--) {
+            rightHighest = Math.max(rightHighest, height[j]);
+            int min = Math.min(leftHighest[j], rightHighest);
+            maxArea += (min > height[j]) ? min - height[j] : 0;
+        }
 
-		return maxArea;
-	}
+        return maxArea;
+    }
 
-	// Solution 2: Stack
-	public static int trap2(int[] height) {
-		if (height == null || height.length == 0)
-			return 0;
+    // Solution 1: Two Pointers
+    public static int trap1(int[] height) {
+        if (height == null || height.length == 0)
+            return 0;
+        int i = 0, j = height.length - 1;
+        int leftMax = height[i], rightMax = height[j];
+        int maxArea = 0;
 
-		int maxArea = 0;
-		Stack<Integer> st = new Stack<>();
-		int i = 0;
-		while (i < height.length) {
-			if (st.isEmpty() || height[i] <= height[st.peek()]) {
-				st.push(i++);
-			} else {
-				// TODO: Think it over
-				int bar = st.pop();
-				int tmp = st.isEmpty() ? 0
-						: (Math.min(height[st.peek()], height[i]) - height[bar]) * (i - st.peek() - 1);
+        while (i < j) {
+            if (height[i] < height[j]) {
+                leftMax = Math.max(height[i], leftMax);
+                maxArea += leftMax - height[i];
+                i++;
+            } else {
+                rightMax = Math.max(height[j], rightMax);
+                maxArea += rightMax - height[j];
+                j--;
+            }
+        }
 
-				maxArea += tmp;
-			}
-		}
+        return maxArea;
+    }
 
-		return maxArea;
-	}
+    // Solution 2: Stack
+    public static int trap2(int[] height) {
+        if (height == null || height.length == 0)
+            return 0;
 
-	public static void main(String[] args) {
-		int[] a = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
-		System.out.println(trap2(a));
-	}
+        int maxArea = 0;
+        Stack<Integer> st = new Stack<>();
+        int i = 0;
+        while (i < height.length) {
+            if (st.isEmpty() || height[i] <= height[st.peek()]) {
+                st.push(i++);
+            } else {
+                int bar = st.pop();
+                int tmp = st.isEmpty() ? 0
+                    : (Math.min(height[st.peek()], height[i]) - height[bar]) * (i - st.peek() - 1);
+
+                maxArea += tmp;
+            }
+        }
+
+        return maxArea;
+    }
 }
