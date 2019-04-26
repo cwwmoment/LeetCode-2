d9176343ad37cb4f9a1e774179a97f67e4877ab8
happygirlzt <happygirlzt@gmail.com>
2019-04-26 14:19:37 +0800

    day 43

--- Java/CombinationSum.java
+++ Java/CombinationSum.java
@@ -10,6 +10,29 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CombinationSum {
+    // Updated on 26 Apr 2019
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        helper(res, new ArrayList<>(), candidates, target, 0);
+        return res;
+    }
+
+    private void helper(List<List<Integer>> res, List<Integer> tmp, int[] candidates, int target, int index) {
+        if (target <= 0) {
+            if (target == 0) {
+                res.add(new ArrayList<>(tmp));
+            }
+
+            return;
+        }
+
+        for (int i = index; i < candidates.length; i++) {
+            tmp.add(candidates[i]);
+            helper(res, tmp, candidates, target - candidates[i], i);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
         if (candidates.length == 0) return res;
--- Java/CombinationSum2.java
+++ Java/CombinationSum2.java
@@ -1,7 +1,6 @@
-
 /**
  * LeetCode #40. Combination Sum II
- * 
+ *
  * Created by happygirlzt on 30 Aug 2018 at 10:10:39 AM
  *
  */
@@ -11,35 +10,53 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CombinationSum2 {
-	/**
-	 * 这题和#39差不多，只有一点细微的差别，在递归那里start需要增1
-	 * 还需要判断是否tmp已经存在res中了。
-	 * @param candidates
-	 * @param target
-	 * @return
-	 */
-	public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-		List<List<Integer>> res = new ArrayList<>();
-		if (candidates.length == 0)
-			return res;
+    // Updated on 26 Apr 2019
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(candidates);
-		backtrack(res, new ArrayList<>(), candidates, target, 0);
-		return res;
-	}
+        helper(res, candidates, new ArrayList<>(), target, 0);
+        return res;
+    }
 
-	public void backtrack(List<List<Integer>> res, ArrayList<Integer> tmp, int[] nums, int remain, int start) {
-		if (remain < 0) {
-			return;
-		} else if (remain == 0) {
-			if (!res.contains(tmp)) {
-				res.add(new ArrayList<>(tmp));
-			}
-		} else {
-			for (int i = start; i < nums.length; i++) {
-				tmp.add(nums[i]);
-				backtrack(res, tmp, nums, remain - nums[i], i + 1);
-				tmp.remove(tmp.size() - 1);
-			}
-		}
-	}
+    private void helper(List<List<Integer>> res, int[] candidates, List<Integer> tmp, int target, int index) {
+        if (target <= 0) {
+            if (target == 0) {
+                res.add(new ArrayList(tmp));
+            }
+            return;
+        }
+
+        for (int i = index; i < candidates.length; i++) {
+            if (i > index && candidates[i] == candidates[i - 1]) continue;
+            tmp.add(candidates[i]);
+            helper(res, candidates, tmp, target - candidates[i], i + 1);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+
+    // 30 Aug 2018
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (candidates.length == 0)
+            return res;
+        Arrays.sort(candidates);
+        backtrack(res, new ArrayList<>(), candidates, target, 0);
+        return res;
+    }
+
+    public void backtrack(List<List<Integer>> res, ArrayList<Integer> tmp, int[] nums, int remain, int start) {
+        if (remain < 0) {
+            return;
+        } else if (remain == 0) {
+            if (!res.contains(tmp)) {
+                res.add(new ArrayList<>(tmp));
+            }
+        } else {
+            for (int i = start; i < nums.length; i++) {
+                tmp.add(nums[i]);
+                backtrack(res, tmp, nums, remain - nums[i], i + 1);
+                tmp.remove(tmp.size() - 1);
+            }
+        }
+    }
 }
