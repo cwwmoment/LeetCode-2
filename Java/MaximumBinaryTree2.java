a4d31250b751a1144ede91a6c91eaaa3918d86d4
happygirlzt <happygirlzt@gmail.com>
2019-04-28 20:20:44 +0800

    day 45

---
+++ Java/MaximumBinaryTree2.java
@@ -0,0 +1,28 @@
+/**
+ * Created on 28 Apr 2019 by happygirlzt
+ *
+ * LeetCode #998. Maximum Binary Tree II
+ *
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        if (root != null && root.val > val) {
+            root.right = insertIntoMaxTree(root.right, val);
+            return root;
+        }
+
+        TreeNode node = new TreeNode(val);
+        node.left = root;
+        return node;
+    }
+}
