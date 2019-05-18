23c3bb59a2f71097c7c90c89a52826b6e469be8a
happygirlzt <happygirlzt@gmail.com>
2019-05-18 21:07:25 +0800

    day 65

--- Java/ImplementStrStr.java
+++ Java/ImplementStrStr.java
@@ -6,6 +6,51 @@
  */
 
 public class ImplementStrStr {
+    // Updated on 28 May 2019
+    // KMP Algorithm
+    public int strStr(String t, String p) {
+        int n = t.length();
+        int m = p.length();
+        if (m == 0) return 0;
+
+        int[] pi = new int[m];
+        computePrefix(p, pi);
+
+        int k = -1;
+        for (int i = 0; i < n; i++) {
+            while (k >= 0 && p.charAt(k + 1) != t.charAt(i)) {
+                k = pi[k];
+            }
+
+            if (p.charAt(k + 1) == t.charAt(i)) {
+                k++;
+            }
+
+            if (k == m - 1) {
+                return i - m + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    private void computePrefix(String p, int[] pi) {
+        pi[0] = -1;
+        int k = -1;
+        int m = p.length();
+        for (int i = 1; i < m; i++) {
+            while (k >= 0 && p.charAt(k + 1) != p.charAt(i)) {
+                k = pi[k];
+            }
+
+            if (p.charAt(k + 1) == p.charAt(i)) {
+                k++;
+            }
+
+            pi[i] = k;
+        }
+    }
+
     public static int strStr(String haystack, String needle) {
         if (needle == null || needle.length() == 0)
             return 0;
@@ -34,12 +79,5 @@ public class ImplementStrStr {
         }
 
         return -1;
-    }
-
-    public static void main(String[] args) {
-        String s1 = "hello";
-        String s2 = "ll";
-
-        System.out.println(strStr(s1, s2));
     }
 }
